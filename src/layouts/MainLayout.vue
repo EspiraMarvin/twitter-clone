7e76610074a8b62395e72f37dcfaab66dd4eeaca
Marvin Espira <espiramarvin@gmail.com>
2021-03-10 18:59:30 +0300

    added header for mobile and larger devices

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,13 +8,25 @@
         <q-toolbar-title
           class="text-weight-bold"
         >
-          Qwitter
+          <span class="gt-sm">Qwitter</span>
+          <q-icon
+            class="q-pa-md header-icon"
+            name="fas fa-dove lt-md"
+            size="sm"
+            color="primary"
+          />
         </q-toolbar-title>
 
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
+    <q-drawer
+      :width="283"
+      show-if-above
+      v-model="left"
+      side="left"
+      bordered
+    >
       <q-icon
         name="fas fa-dove"
         size="lg"
@@ -22,18 +34,87 @@
         class="q-pa-md"
       />
       <q-list>
-        <q-item clickable v-ripple>
+        <q-item
+          to="/"
+          clickable
+          v-ripple
+        >
           <q-item-section avatar>
-            <q-icon color="primary" name="home" />
+            <q-icon name="home" size="md" />
           </q-item-section>
 
-          <q-item-section>Home</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item
+          to="/about"
+          clickable
+          v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
 
-    <q-drawer show-if-above v-model="right" side="right" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above v-model="right" side="right" bordered>
+      <q-input
+        placeholder="Search..."
+        class="q-ma-md"
+        outlined
+        rounded
+        dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list
+        padding
+        separator
+      >
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">EDUCATION</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happened!</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">FOOTBALL</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happened!</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">POLITICS</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happened!</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+
     </q-drawer>
 
     <q-page-container>
@@ -53,3 +134,11 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .header-icon
+    position: absolute
+    bottom: 0
+    left: 50%
+    transform: translateX(-50%)
+</style>
