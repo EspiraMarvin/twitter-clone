f70a673cb6779aa9944e4f1674da11f039eed551
Marvin Espira <espiramarvin@gmail.com>
2021-04-15 00:14:35 +0300

    created add tweet component

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,36 +1,43 @@
 <template>
   <q-page>
-    <div class="q-py-lg q-px-md">
-      <q-input
-        bottom-slots
-        v-model="newQweetContent"
-        placeholder="What's happening?"
-        maxlength="280"
-        counter
-        autogrow
-      >
-        <template v-slot:before>
-          <q-avatar size="xl">
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
-          </q-avatar>
-        </template>
+    <div class="q-py-lg q-px-md row items-end  q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQweetContent"
+          class="new-qweet"
+          placeholder="What's happening?"
+          maxlength="280"
+          counter
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
 
-        <template v-slot:hint>
-          Field hint
-        </template>
-
-        <template v-slot:after>
-          <q-btn
-            :disable="!newQweetContent"
-            unelevated
-            rounded
-            color="primary"
-            label="Qweet"
-            no-caps
-          />
-        </template>
-      </q-input>
+      <div class="col col-shrink">
+        <q-btn
+          :disable="!newQweetContent"
+          class="q-mb-lg"
+          unelevated
+          rounded
+          color="primary"
+          label="Qweet"
+          no-caps
+        />
+      </div>
     </div>
+
+    <q-separator
+      size="10px"
+      color="grey-2"
+      class="divider"
+    />
+
   </q-page>
 </template>
 
@@ -44,3 +51,14 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .new-qweet
+    textarea
+      font-size: 19px
+      line-height: 1.4 !important
+  .divider
+    border-top: 1px solid
+    border-bottom: 1px solid
+    border-color: $grey-4
+</style>
