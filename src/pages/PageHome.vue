cfafb6622fc069eb920b5816b3a8de28aff78faa
Marvin Espira <espiramarvin@gmail.com>
2021-04-15 13:03:26 +0300

    added retween feature in frontend

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -72,13 +72,21 @@
             </q-item-label>
             <div class="qweeet-icons row justify-between q-mt-sm">
               <q-btn icon="far fa-comment" size="sm" color="grey" flat round/>
-              <q-btn icon="fas fa-retweet" size="sm" color="grey" flat round/>
+              <q-btn
+                :icon="qweet.retweet ? 'fas fa-retweet' : 'fa fa-retweet'"
+                :color="qweet.retweet ? 'green' : 'grey'"
+                :label="qweet.retweet ? 1 : ''"
+                size="sm"
+                flat round
+              />
               <q-btn
                 @click="toggleLiked(qweet)"
                 :icon="qweet.liked ? 'fas fa-heart' : 'far fa-heart'"
                 :color="qweet.liked ? 'pink' : 'grey'"
+                :label="qweet.liked ? 1 : ''"
                 size="sm"
-                flat round
+                flat
+                round
               />
               <q-btn
                 @click="deleteQweet(qweet)"
@@ -104,20 +112,22 @@ export default {
       newQweetContent: '',
       avatar: 'https://cdn.quasar.dev/img/avatar4.jpg',
       qweets: [
-        // {
-        //   id: 1,
-        //   content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, asperiores autem cum dolor doloremque eos error exercitationem facere iure modi nulla, praesentium provident qui ratione voluptate. Dolores error excepturi vel!\n' +
-        //     '\n' +
-        //     '            Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-        //   date: 1618459322555,
-        //   liked: false
-        // },
-        // {
-        //   id: 2,
-        //   content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium animi aut autem consequatur doloremque doloribus ducimus, eaque eum explicabo fugiat in ipsum iste nemo provident quo quos reprehenderit saepe voluptates.',
-        //   date: 1618459352896,
-        //   liked: true
-        // }
+        {
+          id: 1,
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, asperiores autem cum dolor doloremque eos error exercitationem facere iure modi nulla, praesentium provident qui ratione voluptate. Dolores error excepturi vel!\n' +
+            '\n' +
+            '            Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+          date: 1618459322555,
+          retweet: true,
+          liked: false
+        },
+        {
+          id: 2,
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium animi aut autem consequatur doloremque doloribus ducimus, eaque eum explicabo fugiat in ipsum iste nemo provident quo quos reprehenderit saepe voluptates.',
+          date: 1618459352896,
+          retweet: false,
+          liked: true
+        }
       ]
     }
   },
@@ -131,6 +141,7 @@ export default {
       const newQweet = {
         content: this.newQweetContent,
         date: Date.now(),
+        retweet: false,
         liked: false
       }
       // this.qweets.unshift(newQweet)
@@ -164,29 +175,43 @@ export default {
         })
     }
   },
-  mounted () {
-    db.collection('qweets').orderBy('date').onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        const qweetChange = change.doc.data()
-        qweetChange.id = change.doc.id
-        if (change.type === 'added') {
-          console.log('New qweet: ', qweetChange)
-          this.qweets.unshift(qweetChange)
-        }
-        if (change.type === 'modified') {
-          const index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
-          // Object.assign copies/assigns properties from one or more source Objects to a target Object
-          // In this case, we assign our local data, the Object (this.qweets) to qweetChange object to reflect changes to the UI (this.qweets) at the position Index
-          Object.assign(this.qweets[index], qweetChange)
-          console.log('Modified qweet: ', qweetChange)
-        }
-        if (change.type === 'removed') {
-          console.log('Removed qweet: ', qweetChange)
-          const index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
-          this.qweets.splice(index, 1)
-        }
+  // mounted () {
+  //   db.collection('qweets').orderBy('date').onSnapshot((snapshot) => {
+  //     snapshot.docChanges().forEach((change) => {
+  //       const qweetChange = change.doc.data()
+  //       qweetChange.id = change.doc.id
+  //       if (change.type === 'added') {
+  //         console.log('New qweet: ', qweetChange)
+  //         this.qweets.unshift(qweetChange)
+  //       }
+  //       if (change.type === 'modified') {
+  //         const index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
+  //         // Object.assign copies/assigns properties from one or more source Objects to a target Object
+  //         // In this case, we assign our local data, the Object (this.qweets) to qweetChange object to reflect changes to the UI (this.qweets) at the position Index
+  //         Object.assign(this.qweets[index], qweetChange)
+  //         console.log('Modified qweet: ', qweetChange)
+  //       }
+  //       if (change.type === 'removed') {
+  //         console.log('Removed qweet: ', qweetChange)
+  //         const index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
+  //         this.qweets.splice(index, 1)
+  //       }
+  //     })
+  //   })
+  // },
+  computed: {
+    likedCount () {
+      // return this.qweets.map(qweet => {
+      //   qweet = this.qweet.liked === true ? qweet.liked.length : ''
+      //   return qweet
+      // })
+      return this.qweets.filter(qweet => {
+        // console.log('qweet', qweet)
+        qweet.lable = qweet.liked === true ? 'liked' : 'no'
+        // console.log('like count', qweet)
+        return qweet
       })
-    })
+    }
   }
 }
 </script>
